a8ff177a1534d3d5a1fc845898b0837438de50c3
Carl Clark <carl@caztec.com.au>
2022-07-07 09:21:53 +1000

    Added parameter validation

--- PowerShell/Parameters.ps1
+++ PowerShell/Parameters.ps1
@@ -1,7 +1,10 @@
 # Calling with positional parameters
-# .\Parameters.ps1 MyInstance 10
+# .\Parameters.ps1 MyInstance 10 Yes
 
 # Calling with named parameters
+# .\Parameters.ps1 -retentionDays 20 -instanceName MySecondInstance -mustHave No
+
+# Calling with named parameters parameter validation will fail.
 # .\Parameters.ps1 -retentionDays 20 -instanceName MySecondInstance
 
 param
@@ -10,7 +13,30 @@ param
     [string]$instanceName="default",
 
     # Number of days to retain
-    [int]$retentionDays=5
+    [int]$retentionDays=5,
+
+    [string]$mustHave
 )
 
-Write-Host "Parameter: instanceName=$instanceName retentionDays=$retentionDays"
+# Function Defintions **************************************
+function ValidateParameters
+{
+    # Validate the required parameters and provide a friendly error message
+    if ($mustHave -eq "")
+    {
+        Write-Host "A value must be provided for the -mustHave parameter." -ForegroundColor Red
+        return $False
+    }
+    return $True
+}
+# **********************************************************
+
+$areParametersValid = ValidateParameters
+if ($areParametersValid -ne $True)
+{
+    exit(-1)
+}
+
+Write-Host "Parameter: instanceName=$instanceName retentionDays=$retentionDays mustHave=$mustHave"
+
+
